d18fe10b69d0785e990ea394ccb303cd4a71a241
Jeonghoon Oh <ihoon9203@gmail.com>
2022-09-05 01:08:01 -0600

    first init

---
+++ README.md
@@ -0,0 +1,11 @@
+## Installation
+0. install node.js from https://nodejs.org/en/download/
+1. npm install express -> install express.js
+1. npm install -> to add all dependency
+2. node index.js -> start localhost server: http://localhost:3001/
+
+
+
+##REST API Endpoint
+/all - GET: return all people
+/add - POST: add a new person to document
---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const mongo = require("mongodb").MongoClient;
+const app = express()
+
+const url = 'mongodb+srv://admin:pmvXGKaoInJ0UFgw@testercluster.jepemrd.mongodb.net/test?retryWrites=true&w=majority'
+
+let db, people
+mongo.connect(
+    url,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+    (err, client) => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        db = client.db("people")
+        people = db.collection("people")
+    }
+)
+app.use(express.json())
+app.post("/add", (req, res) => {
+    const name = req.body.name
+    const number = req.body.number
+    const id = req.body.id
+    console.log("adding")
+    console.log(name, number, id)
+    people.insertOne({name: name, number: number, id: id }, (err, result) => {
+        if (err) {
+          console.error(err)
+          res.status(500).json({ err: err })
+          return
+        }
+        console.log(result)
+        res.status(200).json({ ok: true })
+      })
+})
+
+app.get("/all", (req, res) => {
+    people.find().toArray((err,items) => {
+        if (err) {
+            console.error(err)
+            res.status(500).json({ err: err })
+            return
+          }
+          res.status(200).json({ people: items })
+    })
+})
+
+  const PORT = 3001
+  app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`)
+  })
